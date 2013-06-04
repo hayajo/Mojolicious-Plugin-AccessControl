4198b5d37c382be55e66cdb0e19669513a09589d
hayajo <hayajo@cpan.org>
2013-06-05 08:34:35 +0900

    0.03

--- lib/Mojolicious/Plugin/AccessControl.pm
+++ lib/Mojolicious/Plugin/AccessControl.pm
@@ -1,7 +1,7 @@
 package Mojolicious::Plugin::AccessControl;
 use strict;
 use warnings;
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 use Mojo::Base 'Mojolicious::Plugin';
 use Net::CIDR::Lite;
